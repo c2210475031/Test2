996e736a23c1c581d1964116bc6c9f34abd9d700
nicoorth <nico.orth@stud.fh-campuswien.ac.at>
2025-05-30 12:07:22 +0200

    updated imports

--- app/src/main/java/com/example/financetracker/presentation/AddTransactionScreen.kt
+++ app/src/main/java/com/example/financetracker/presentation/AddTransactionScreen.kt
@@ -1,15 +1,37 @@
 package com.example.financetracker.presentation
 
 import android.os.Build
-import android.util.Log
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
 import androidx.compose.material.icons.filled.Home
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Button
+import androidx.compose.material3.DatePicker
+import androidx.compose.material3.DatePickerDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FilterChip
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.rememberDatePickerState
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
@@ -20,15 +42,12 @@ import com.example.financetracker.database.model.Category
 import com.example.financetracker.database.model.CategoryType
 import com.example.financetracker.database.model.Transaction
 import com.example.financetracker.database.repository.TransactionRepository
-import com.example.financetracker.model.TransactionF
 import com.example.financetracker.navigation.Screen
 import com.example.financetracker.viewmodel.TransactionViewModel
 import com.example.financetracker.viewmodel.TransactionViewModelFactory
-import kotlinx.coroutines.flow.StateFlow
 import java.time.Instant
 import java.time.LocalDate
 import java.time.format.DateTimeFormatter
-import kotlin.math.log
 
 @RequiresApi(Build.VERSION_CODES.O)
 @OptIn(ExperimentalMaterial3Api::class)
